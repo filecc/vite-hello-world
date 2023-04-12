591751d60d294732f64ac32e8557e95ab76ea59b
filecc <fil.gia@live.it>
2023-04-12 13:24:58 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <AppTitle />
+  </div>
+</template>
+
+<script>
+  import AppTitle from './components/AppTitle.vue';
+  export default {
+    components: {
+      AppTitle,
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
